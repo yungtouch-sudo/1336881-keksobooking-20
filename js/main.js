94829231882d9e08c1ea8e23fb741307cdc7b493
yungtouch-sudo <yungtouch@yandex.ru>
2020-06-06 17:45:26 +0300

    xxx

--- js/main.js
+++ js/main.js
@@ -1,17 +1,10 @@
 'use strict';
+var OFFERS_NUMBER = 8;
+
 var MAP_WIDTH = 1200;
 var MAP_TOP_Y = 130;
 var MAP_BOTTOM_Y = 630;
 
-var getRandomElement = function (arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
-
-var MinMaxRandom = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
 var ROOM_FROM = 1;
 var ROOM_BEFORE = 8;
 
@@ -36,6 +29,12 @@ var CHECKOUT_TIME = [
   '14:00'
 ];
 
+var PRICE_FROM = [
+  '1000',
+  '2000',
+  '3000'
+];
+
 var FACILITIES = [
   'wifi',
   'dishwasher',
@@ -51,30 +50,90 @@ var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 
-var userSetting = function (settingsList){
-  var positionX = MinMaxRandom(0, MAP_WIDTH);
-  var positionY = MinMaxRandom(MAP_TOP_Y, MAP_BOTTOM_Y);
-  var creator = {
-    author:{
-      avatar: "img/avatars/user0" + (settingsList + 1) + ".png"
+var TITLES = [
+  '1-комн и 2-комн квартиры',
+  '1-комнатные квартиры с балконом',
+  '1-комнатные квартиры на выходные',
+  '1-комнатные квартиры на неделю',
+  '1-комнатные квартиры посуточно'
+];
+var DESCRIPTIONS = [
+  'Снять квартиру',
+  'Cнять команту',
+  'Cнять гараж',
+  'Снять офис',
+  'Снять подвал'
+];
+
+var PIN_X = 25;
+var PIN_Y = 70;
+
+var getRandomElement = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+var minmaxRandom = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+var getOffersList = function () {
+  var offers = [];
+  for (var i = 0; i < OFFERS_NUMBER; i++) {
+    offers[i] = getImageData(i);
+  }
+  return offers;
+};
+
+var getImageData = function (index) {
+  var positionX = minmaxRandom(0, MAP_WIDTH);
+  var positionY = minmaxRandom(MAP_TOP_Y, MAP_BOTTOM_Y);
+  var theCreator = {
+    author: {
+      avatar: 'img/avatars/user0' + (index + 1) + '.png'
     },
     theCreator: {
-      title: "строка, заголовок предложения",
-      address: positionX + ", " + positionY,
-      price: число, стоимость,
+      title: getRandomElement(TITLES),
+      address: positionX + ', ' + positionY,
+      price: getRandomInt(PRICE_FROM),
       type: getRandomElement(USER_CHOICE),
-      rooms: MinMaxRandom(ROOM_FROM, ROOM_BEFORE),
-      guests: MinMaxRandom(GUEST_FROM, GUEST_BEFORE),
+      rooms: minmaxRandom(ROOM_FROM, ROOM_BEFORE),
+      guests: minmaxRandom(GUEST_FROM, GUEST_BEFORE),
       checkin: getRandomElement(CHECKIN_TIME),
       checkout: getRandomElement(CHECKOUT_TIME),
       features: getRandomElement(FACILITIES),
-      description: "строка с описанием",
+      description: getRandomElement(DESCRIPTIONS),
       photos: (PHOTOS)
-},
+    },
     location: {
       x: positionX,
       y: positionY
     }
   };
   return theCreator;
+};
+//getImageData();
+
+var map = document.querySelector('.map');
+activateMap(map);
+
+var activateMap = function (addMap) {
+  addMap.classList.remove('map--faded');
+};
+
+var mapPins = document.querySelector('.map__pins');
+
+var pinTemplate = document.querySelector('#pin');
+
+var activatePin = function (addPin) {
+  var pin = pinTemplate.cloneNode(true);
+  var pinX = addPin.position.x + PIN_X;
+  var pinY = addPin.position.y + PIN_Y;
+  pin.style = 'left: ' + pinX + 'px; top: ' + pinY + 'px;';
+  return pin;
 };
